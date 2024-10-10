3cd5ed05f8e646f530e951a551f88b88768ce0a1
Polvere243 <danielercoli@gmail.com>
2024-10-10 17:15:58 +0200

    trying to figure out js

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
+// dichiaro una funzione che generi un numero casuale tra 0 e 3
+const getRandomNumber = (min, max) => {
+    return Math.floor(Math.random * (max + 1 - 3) + min);
+};
 // dichiaro un array coi colori principali
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+// dichiaro le costanti coi valori minimo e massimo
+const min = 0;
+const max = 3;
 // recupero gli elementi dal DOM
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
+
+// aggancio un ascoltatore di eventi al bottone
+btn.addEventListener('click', () => {
+    document.body.style.backgroundColor = colors[getRandomNumber];
+    color.innerText = colors[getRandomNumber];
+})
