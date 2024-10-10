f40c9a8a919d96058b24c15738e142961233a78e
Polvere243 <danielercoli@gmail.com>
2024-10-10 11:22:39 +0200

    got button from the DOM

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 // dichiaro un array coi colori principali
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 // recupero gli elementi dal DOM
+const btn = document.getElementById('btn');
