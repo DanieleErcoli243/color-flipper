79d6fa318d2d094e5ca58d9270ecc8ee148370a4
Polvere243 <danielercoli@gmail.com>
2024-10-11 11:12:44 +0200

    used a loop to generate the hex code and declared a function to generate random numbers according to the array values

--- hex.js
+++ hex.js
@@ -1,3 +1,5 @@
+// dichiaro una funzione per generare numeri casuali
+const getRandomNumber = () => Math.floor(Math.random() * hex.length);
 // dichiaro un array con interi e caratteri
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 // recupero gli elementi dal DOM
@@ -8,4 +10,9 @@ const color = document.querySelector('.color');
 btn.addEventListener('click', () => {
     // dichiaro una variabile di partenza con la stringa del codice colore
     let hexColor = '#';
+    // faccio un ciclo sull'array per avere i sei caratteri necessari
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[getRandomNumber()];
+    };
+
 })
