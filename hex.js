3d2368add2e5875e7743dacf4a556a25262c8227
Polvere243 <danielercoli@gmail.com>
2024-10-11 10:47:30 +0200

    added a comment and copypasted the code to get the elements from the DOM

--- hex.js
+++ hex.js
@@ -1,1 +1,5 @@
+// dichiaro un array con interi e caratteri
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+// recupero gli elementi dal DOM
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
