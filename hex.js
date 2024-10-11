e219d1bba6a1dfd80531ba5c397109f357ae3c4a
Polvere243 <danielercoli@gmail.com>
2024-10-11 10:49:51 +0200

    added an event listener still to fill

--- hex.js
+++ hex.js
@@ -3,3 +3,8 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 // recupero gli elementi dal DOM
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
+
+// aggancio un ascoltatore di eventi al bottone
+btn.addEventListener('click', () => {
+
+})
